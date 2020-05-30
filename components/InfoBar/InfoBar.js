deb02b6e57e5269bb8f04a77ebeafe17d408e5a2
Pavan Singhal <60385486+Pavan1998singhal@users.noreply.github.com>
2020-05-30 10:22:29 +0530

    Add files via upload

---
+++ components/InfoBar/InfoBar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import closeIcon from '../../Icons/closeIcon.png';
+import onlineIcon from '../../Icons/onlineIcon.png';
+//import './Inforbar.css';
+import './InfoBar.css';
+
+const InfoBar = ({room}) => (
+    <div className="infoBar">
+        <div className="leftInnerContainer">
+            <img className="onlineIcon" src={onlineIcon} alt="online" />
+            <h3>{room}</h3>
+        </div>
+        <div className="rightInnerContainer">
+        <a href="/"><img className="closeIcon" src={closeIcon} alt="close" /></a>
+        </div>
+    </div>
+);
+
+export default InfoBar;
